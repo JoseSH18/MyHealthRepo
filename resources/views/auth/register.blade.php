74fe071c073e55f7bdb25f3af33a83021eb6b9d2
JoseSH18 <josepsr18@gmail.com>
2022-10-11 16:48:27 -0600

    Español confirmaciones de registro y login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,7 +61,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="role" class="col-md-4 col-form-label text-md-end">{{ __('Role') }}</label>
+                            <label for="role" class="col-md-4 col-form-label text-md-end">{{ __('Rol') }}</label>
 
                             <div class="col-md-6">
                                 
