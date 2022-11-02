15f2bd05c6cda93a4131498ce5f07c5b67dc88b5
MoisesSegura <moisessegurarojas@gmail.com>
2022-11-02 00:27:03 -0600

    se agrego  editar niveles de azucar

--- app/Http/Controllers/PacientesController.php
+++ app/Http/Controllers/PacientesController.php
@@ -138,12 +138,12 @@ class PacientesController extends Controller
 
     public function updateSugar(Request $request, $id){
         $user = Auth::user();
-        $patients = patient::firstWhere('correo', $user->email);
         $sugar= sugar::find($id);
 
         $sugar->valor = $request->valor;
         $sugar->fecha = $request->fecha;
         $sugar->save();
+        return redirect()->back();
     }
 
     public function deleteSugar(Request $request, $id){
--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::post('/paciente/{cedula_paciente}/update', [App\Http\Controllers\Paciente
 
 Route::get('/paciente/grafica_de_Azucar', [App\Http\Controllers\PacientesController::class, 'grafica_de_Azucar'])->name('paciente.grafica_de_Azucar');
 Route::post('/paciente/store', [App\Http\Controllers\PacientesController::class, 'store'])->middleware('can:paciente.grafica_de_Azucar')->name('paciente.store');
-Route::post('/paciente/{cedula_paciente}/update', [App\Http\Controllers\PacientesController::class, 'updateSugar'])->middleware('can:paciente.grafica_de_Azucar')->name('paciente.updateSugar');
+Route::post('/paciente/{cedula_paciente}/updateSugar', [App\Http\Controllers\PacientesController::class, 'updateSugar'])->middleware('can:paciente.grafica_de_Azucar')->name('paciente.updateSugar');
 Route::delete('/paciente/{cedula_paciente}/deleteSugar', [App\Http\Controllers\PacientesController::class, 'deleteSugar'])->middleware('can:paciente.grafica_de_Azucar')->name('paciente.deleteSugar');
 
 Route::get('/paciente/vista_recordatorio', [App\Http\Controllers\PacientesController::class, 'vista_recordatorio'])->name('paciente.vista_recordatorio');
