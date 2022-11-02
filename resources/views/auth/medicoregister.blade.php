d8fc2f7fa41cb2093be69c2e45b91d3ca2cabfbd
JoseSH18 <josepsr18@gmail.com>
2022-11-01 23:07:28 -0600

    Cambios pequeños errores

--- resources/views/auth/medicoregister.blade.php
+++ resources/views/auth/medicoregister.blade.php
@@ -25,7 +25,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nombre') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Primer Nombre') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
