405f5cbda0b2967d2de817046b8d4e0f302b5bce
JoseSH18 <josepsr18@gmail.com>
2022-11-18 17:15:29 -0600

    mejora contraseña

--- resources/views/auth/medicoregister.blade.php
+++ resources/views/auth/medicoregister.blade.php
@@ -129,7 +129,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Ingrese contraseña de mínimo 8 caracteres" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
